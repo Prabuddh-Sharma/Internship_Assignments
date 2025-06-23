f0c1ebc15f487ac5a025a7c7111701ee8014397e
Prabuddh Sharma <prabuddhsharma7@gmail.com>
2025-06-23 08:09:45 +0530

    Completed Day 3 [June 21, 2025] Assignment_3

---
+++ Assignment_3.py
@@ -0,0 +1,42 @@
+
+#Assignment 3.1
+
+def cal():
+    
+    print("Basic Math Operations ->")
+
+    n1 = float(input("Enter the first number: "))
+    n2 = float(input("Enter the second number: "))
+
+    print(f"\nAddition: {n1} + {n2} = {n1 + n2}")
+    print(f"Subtraction: {n1} - {n2} = {n1 - n2}")
+    print(f"Multiplication: {n1} * {n2} = {n1 * n2}")
+
+    if num2 == 0:
+        print(f"Division: {n1} / {n2} Error")
+    else:
+        print(f"Division: {n1} / {n2} = {n1 / n2}")
+
+# TO EXECUTE ->
+#cal()
+
+#Assignment 3.2
+
+def Palindrome(x):
+    
+    if x < 0:
+        return False
+
+    s = str(x)
+
+    r_str = ""
+    for char in s:
+        r_str = char + r_str
+
+    return s == r_str
+
+print(f"FOR 121  {Palindrome(121)}")
+print(f"FOR -121 {Palindrome(-121)}")
+print(f"FOR 10 {Palindrome(10)}")
+print(f"FOR 12321 {Palindrome(12321)}")
+print(f"FOR 0 {Palindrome(0)}")
